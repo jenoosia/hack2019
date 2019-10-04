4de22d067e06d3ca4a493b3056ca266866f756b0
Jensen Ching <jensen.ching@enovax.com>
2019-10-04 14:08:50 +0800

    Initial Spring Boot project

---
+++ core-backend/src/main/java/com/exaz/hack2019/CoreApp.java
@@ -0,0 +1,13 @@
+package com.exaz.hack2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApp.class, args);
+    }
+
+}
