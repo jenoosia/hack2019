449710721e95efada0b348cfe77bc5d56e11f205
Jensen Ching <jensen.ching@enovax.com>
2019-10-04 17:40:38 +0800

    Added a mock whatsapp webhook endpoint

---
+++ core-backend/src/main/java/com/exaz/hack2019/konsulta/WhatsappController.java
@@ -0,0 +1,27 @@
+package com.exaz.hack2019.konsulta;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class WhatsappController {
+
+    private final Logger log = LogManager.getLogger(getClass().getName());
+
+    @RequestMapping(path = "/hooks/whatsapp", method = RequestMethod.POST, produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> processHook(@RequestBody String kvpInput) {
+        log.info(kvpInput);
+        return new ResponseEntity<>("You said something I don't understand, 'cause I'm still a basic bot at the moment." , HttpStatus.OK);
+    }
+
+}
